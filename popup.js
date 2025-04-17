a86e3af0e782acc83835477c5bd7aeb536ae6e76
Devin-Putnam <dputnam@fandm.edu>
2025-04-17 17:37:19 -0400

    Delete popup.js

--- popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("send").addEventListener("click", async () => {
-  const res = await fetch("http://127.0.0.1:5000/api/hello");
-  const data = await res.json();
-  document.getElementById("response").innerText = data.message;
-});
