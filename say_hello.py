68974cae037113da06283502422db28c2a760e8b
Devin-Putnam <dputnam@fandm.edu>
2025-04-17 17:37:28 -0400

    Delete say_hello.py

--- say_hello.py
+++
@@ -1,7 +0,0 @@
-import sublime
-import sublime_plugin
-
-class SayHelloCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.insert(edit, 0, "Hello from your Sublime plugin!\n")
-
